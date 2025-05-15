716e0c2b2682b1abefd9c7c5590ff4bc723fb1b3
muharief112 <muhammadarief379@gmail.com>
2025-05-15 11:05:54 +0700

    Create User Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +16,19 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+
+        // Buat role jika belum ada
+        $adminRole = Role::firstOrCreate(['name' => 'hr']);
+
+
+        $user = User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => bcrypt('password')
         ]);
+
+
+        // Assign role ke user
+        $user->assignRole($adminRole);
     }
 }
